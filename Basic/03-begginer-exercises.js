e98702af3c762716a2ee4bf338e980817c2fec47
Silvana <user@DESKTOP-EF46R3H>
2025-06-09 16:21:42 -0400

    del 1 al 3 con los eercicos minuto 1.20.42(Curso javaScript)

---
+++ Basic/03-begginer-exercises.js
@@ -0,0 +1,50 @@
+// 1. Escribe un comentario en una línea
+
+//hola
+
+// 2. Escribe un comentario en varias líneas
+/*
+ Hola
+silvana 
+sooy 
+yo
+*/
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let myname = "Silvana"
+let age = 19 
+let isTeacher= false
+let undefinedValue
+let myBigInt= BigInt(21236155333333333333333333333333111111111111612320354864001)
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(myname)
+console.log(age)
+console.log(isTeacher)
+console.log(undefinedValue)
+console.log(myBigInt)
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof myname)
+console.log(typeof age)
+console.log(typeof isTeacher)
+console.log(typeof undefinedValue)
+console.log(typeof myBigInt)
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+let myname2 = "Silvana pere"
+let age2 = 192 
+let isTeacher2= true
+let undefinedValue2
+let myBigInt2= BigInt(30)
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+let myname21 = 2
+let age21 = 'hola' 
+let isTeacher21= true
+let undefinedValue21
+let myBigInt21= 30
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
