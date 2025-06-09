6e07061eff52169ed871d8d12c8f3ee0ff0b77e0
Silvana <user@DESKTOP-EF46R3H>
2025-06-09 15:59:25 -0400

    del 1 al 3 con los eercicos minuto 1.20.42(Curso javaScript)

---
+++ Basic/01-variables.js
@@ -0,0 +1,22 @@
+//var
+
+var helloWorld = "Hello JavaScript"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo"
+console.log(helloWorld)
+
+//let
+let helloWorld2 = "Hello JavaScript2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hello JavaScript23"
+console.log(helloWorld2)
+
+//const
+
+const helloWorld3 = "Hello JavaScript3"
+console.log(helloWorld3)
+
+// helloWorld3 = "Hello JavaScript3"
+// console.log(helloWorld3)
---
+++ Basic/02-datatype.js
@@ -0,0 +1,59 @@
+//Tipos de datos primitivos
+
+//String (cadena de texto)
+let myname = "Silvana"
+let alias ='Florangel'
+let email = `silvana@pere.com`
+
+//Numeros
+let age = 19 //Entero
+let heigt = 1.60 //Decimal
+
+//Booleanos (boolean)
+let isStudent= true
+let isTeacher= false
+
+//Undefined
+let undefinedValue
+//console.log(undefinedValue)
+
+//null
+let nullValue = null
+
+//Symbol
+
+let mySymbol = Symbol("mySymbol")
+
+//BigInt
+
+let myBigInt= BigInt(21236155333333333333333333333333111111111111612320354864001)
+let myBigInt2=21236155333333333333333333333333111111111111612320354864001n
+
+//Mostrar tipos de datos
+console.log(typeof myname)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof heigt)
+
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof isTeacher)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
+
+
+
+
+
+
+
