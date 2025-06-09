22776bc8e46b3d091187c6a81b1594f4b7632d2f
Silvana <user@DESKTOP-EF46R3H>
2025-06-09 16:45:09 -0400

    OperAdosEaes minuto 1.30.42(Curso javaScript)

---
+++ Basic/04-operators.js
@@ -0,0 +1,67 @@
+//operadores
+
+//operadores aritmeticos
+
+let a = 5
+let b = 10
+
+console.log(a+b) //suma
+console.log(a-b)//resta
+console.log(a/b)//division
+console.log(a*b)//multiplicacion 
+
+console.log(a%b)//Modulo
+console.log(a**b)//exponente
+
+a++ //incremento
+console.log(a)
+
+b--//decremento
+console.log(b--)
+
+//Operadores de asignacion
+
+let myVariable = 2 //(= es el que asigna)
+console.log(myVariable)
+myVariable += 2
+console.log(myVariable)
+
+myVariable += 2
+myVariable -= 2
+myVariable *= 2
+myVariable /= 2
+myVariable %= 2
+myVariable **= 2
+
+//Operadores de comparacion
+
+console.log(a>b)
+console.log(a<b)
+console.log(10>=10)
+console.log(a<=b)
+console.log(a==6) //igualdad por valor
+console.log(a === '6')
+console.log(a==a)
+console.log(a===a)
+console.log(a != 6)
+console.log(a !== '6')
+
+console.log(0 == false, '0 ')
+console.log(1 == false, '1 ')
+console.log(2 == false, '2')
+console.log(0 == "")
+console.log(" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
