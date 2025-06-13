6225d6948b9eff195cd5aedfb45a497cf9c3c247
Silvana <user@DESKTOP-EF46R3H>
2025-06-13 10:34:37 -0400

    condicionales

--- Basic/09-condicional-exercise.js
+++ Basic/09-condicional-exercise.js
@@ -1,11 +1,37 @@
 
 // if/else/else if/ternaria
-
+let nombre = 'silvana'
 // 1. Imprime por consola tu nombre si una variable toma su valor
+if(nombre == 'silvana'){
+    console.log(nombre)
+}
 
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
 
+// let user = 'Carlos'
+// let pass = '123'
+
+// let Inputuser = prompt("Ingresa el usuario")
+
+// let InputPass = prompt("Ingresa la clave")
+
+// if(Inputuser === Inputuser && InputPass===InputPass){
+//     console.log("Credenciales correctas")
+// }else{console.log("credenciales incorrectas")}
+
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
+
+let n1 = 0
+let n2 = 0
+let n3 = -4
+
+if(n1 >= 1){
+console.log('Es positivo')
+}else if(n2 < -1 && n2 >1){
+console.log('es cero')
+}else if(n3 >=-1){
+    console.log(" es Negativo")
+}
 
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
 
